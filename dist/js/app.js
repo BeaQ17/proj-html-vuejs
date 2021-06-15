36cebc0f406b96f3f33f90dc1128082f89b59986
BeaQ17 <beaq235@gmail.com>
2021-06-16 00:42:29 +0200

    save point 3 -header done

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,6 +8,38 @@
 /***/ (() => {
 
 //alert("test alert")
+//jumbotron slideshow
+var slideshow = new Vue({
+  el: "#slideshow",
+  data: {
+    counter: 0,
+    divs: ["#first_div", "#second_div"]
+  },
+  methods: {
+    slidePrev: function slidePrev() {
+      console.log("you clicked prev");
+
+      if (this.counter === 0) {
+        return this.divs.length - 1;
+      }
+
+      return this.counter -= 1;
+    },
+    slideNext: function slideNext() {
+      console.log("you clicked next");
+
+      if (this.counter === this.divs.length - 1) {
+        return this.counter = 0;
+      }
+
+      return this.counter += 1;
+    }
+  },
+  mounted: function mounted() {
+    //slide interval
+    setInterval(this.next, 6000);
+  }
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 //alert("test alert")
+
+//jumbotron slideshow
+const slideshow = new Vue ({
+    el: "#slideshow",
+    data: {
+        counter: 0,
+        divs: [
+            "#first_div",
+            "#second_div"
+        ]
+
+    },
+    methods: {
+        slidePrev(){
+            console.log("you clicked prev");
+            
+            if(this.counter === 0){
+                return this.divs.length - 1;
+            }
+            return this.counter -= 1;
+        },
+        slideNext(){
+            console.log("you clicked next");
+            if (this.counter === this.divs.length - 1) {
+                return this.counter = 0;
+            }
+            return this.counter += 1;
+        }
+    },
+    mounted(){
+
+        //slide interval
+        setInterval(this.next, 6000)
+
+    }
+}) 
